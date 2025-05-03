4b804ba17d0a4a3090440420e641f8b0fd1296f9
yash978 <yashwardhansoni953@gmail.com>
2025-05-03 12:06:43 +0530

    first commit

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { loginUser, registerUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment,  paymentStripe, verifyStripe } from '../controllers/userController.js';
+import upload from '../middleware/multer.js';
+import authUser from '../middleware/authUser.js';
+const userRouter = express.Router();
+
+userRouter.post("/register", registerUser)
+userRouter.post("/login", loginUser)
+
+userRouter.get("/get-profile", authUser, getProfile)
+userRouter.post("/update-profile", upload.single('image'), authUser, updateProfile)
+userRouter.post("/book-appointment", authUser, bookAppointment)
+userRouter.get("/appointments", authUser, listAppointment)
+userRouter.post("/cancel-appointment", authUser, cancelAppointment)
+userRouter.post("/payment-stripe", authUser, paymentStripe)
+userRouter.post("/verifyStripe", authUser, verifyStripe)
+
+export default userRouter;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,71 @@
+import React, { useContext, useState } from 'react'
+import { assets } from '../assets/assets'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+
+const Navbar = () => {
+
+  const navigate = useNavigate()
+
+  const [showMenu, setShowMenu] = useState(false)
+  const { token, setToken, userData } = useContext(AppContext)
+
+  const logout = () => {
+    localStorage.removeItem('token')
+    setToken(false)
+    navigate('/login')
+  }
+
+  return (
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-[#ADADAD]'>
+      <img onClick={() => navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt="" />
+      <ul className='md:flex items-start gap-5 font-medium hidden'>
+        <NavLink to='/' >
+          <li className='py-1'>HOME</li>
+        </NavLink>
+        <NavLink to='/doctors' >
+          <li className='py-1'>ALL DOCTORS</li>
+        </NavLink>
+        <NavLink to='/about' >
+          <li className='py-1'>ABOUT</li>
+        </NavLink>
+        <NavLink to='/contact' >
+          <li className='py-1'>CONTACT</li>
+        </NavLink>
+        <a
+          href="http://localhost:5174/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="px-4 py-1 border rounded-full text-sm text-black hover:bg-gray-100 flex items-center"
+        >
+          Admin Panel
+        </a>
+      </ul>
+
+      <div className='flex items-center gap-4'>
+
+
+        {
+          token && userData
+            ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+              <img className='w-8 rounded-full' src={userData.image} alt="" />
+              <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+              <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                <div className='min-w-48 bg-gray-50 rounded flex flex-col gap-4 p-4'>
+                  <p onClick={() => navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                  <p onClick={() => navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                  <p onClick={logout} className='hover:text-black cursor-pointer'>Logout</p>
+                </div>
+              </div>
+            </div>
+            : <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light'>Create account</button>
+        }
+
+        <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+      </div>
+    </div>
+  )
+
+}
+
+export default Navbar
